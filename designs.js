2413db40bd80fd172f1ad0d5df607ff37a7d1da4
Dan <danixue@gmail.com>
2022-11-10 18:00:44 -0500

    finished project

--- designs.js
+++ designs.js
@@ -1,10 +1,30 @@
-// Select color input
-// Select size input
+function makeGrid(event) {
+  event.preventDefault();
+  const height = event.target[0].value;
+  const width = event.target[1].value;
 
-// When size is submitted by the user, call makeGrid()
+  console.log('this is the num of rows', height);
+  console.log('this is the num of columns', width);
 
-function makeGrid() {
+  const grabTable = document.getElementById('pixelCanvas');
 
-// Your code goes here!
+  if (grabTable.innerHTML !== '') {
+    grabTable.innerHTML = '';
+  }
 
+  for (let i = 0; i < height; i++) {
+    const row = grabTable.insertRow(i);
+    for (let j = 0; j < width; j++) {
+      const cell = row.insertCell(j);
+    }
+  }
+
+  const pixels = document.getElementsByTagName('td');
+
+  console.log('this is the pixels object', pixels);
+  for (let i = 0; i < pixels.length; i++) {
+    pixels[i].addEventListener('click', () => {
+      pixels[i].style.backgroundColor = document.getElementById('colorPicker').value;
+    });
+  }
 }
